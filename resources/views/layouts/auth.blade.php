cb055b289325192d782f9b5a8ef682c18758bce0
Utibe-Abasi Emmanuel <utyemma@gmail.com>
2024-11-02 15:53:14 +0100

    Fixed

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout title="{{$title}}" >
     <div class="d-flex flex-column flex-root">
         <div class="d-flex flex-column flex-lg-row flex-column-fluid">
-            <div class="d-flex flex-column flex-lg-row-fluid w-lg-50 p-10 order-2 order-lg-1">
+            <div class="d-flex flex-column flex-lg-row-fluid w-lg-50 p-md-10 p-4 order-2 order-lg-1">
                 <div class="d-flex flex-center flex-column flex-lg-row-fluid">
                     <div class="w-lg-500px p-10 card">
                         <div class="mb-5 d-flex justify-content-center">
