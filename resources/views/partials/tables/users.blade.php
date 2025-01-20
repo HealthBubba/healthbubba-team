fc3861b2b0cf2eb80fb482021a74d5572ab84d53
Utibe-Abasi Emmanuel <utyemma@gmail.com>
2025-01-20 09:44:39 +0100

    Added Account

--- resources/views/partials/tables/users.blade.php
+++ resources/views/partials/tables/users.blade.php
@@ -27,7 +27,7 @@
                         <div>
                             <!--begin::Toggle-->
                             <button type="button" class="btn btn-light btn-icon btn-sm rotate" data-kt-menu-trigger="click" data-kt-menu-placement="bottom-end" data-kt-menu-offset="10px, 10px">
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-10px h-10px">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-20px h-20px">
                                     <path stroke-linecap="round" stroke-linejoin="round" d="M12 6.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5ZM12 12.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5ZM12 18.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5Z" />
                                   </svg>                                  
                             </button>
