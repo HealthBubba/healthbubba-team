f3e79c6c8a79f6c5523b1624b88f804a2b5bc83f
Utibe-Abasi Emmanuel <utyemma@gmail.com>
2025-01-20 09:43:42 +0100

    Added Account

--- resources/views/partials/tables/users.blade.php
+++ resources/views/partials/tables/users.blade.php
@@ -26,8 +26,8 @@
                     <td>
                         <div>
                             <!--begin::Toggle-->
-                            <button type="button" class="btn btn-light btn-icon btn-sm rotate" data-kt-menu-trigger="click" data-kt-menu-placement="bottom-start" data-kt-menu-offset="30px, 30px">
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-4">
+                            <button type="button" class="btn btn-light btn-icon btn-sm rotate" data-kt-menu-trigger="click" data-kt-menu-placement="bottom-end" data-kt-menu-offset="10px, 10px">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-10px h-10px">
                                     <path stroke-linecap="round" stroke-linejoin="round" d="M12 6.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5ZM12 12.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5ZM12 18.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5Z" />
                                   </svg>                                  
                             </button>
@@ -49,7 +49,7 @@
                                     </a>
                                 </div>
                                 <div class="menu-item px-3">
-                                    <x-swal href="{{route('users.destroy', ['user' => $user->id])}}" class="btn btn-sm btn-icon btn-light-danger w-30px h-30px">
+                                    <x-swal href="{{route('users.destroy', ['user' => $user->id])}}" class="menu-link px-3">
                                         Delete
                                     </x-swal>
                                 </div>
